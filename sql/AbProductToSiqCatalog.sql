c8282f6bfc445b456f88a707d42d8a47b3d75102
David Purdy <dnpurdy@gmail.com>
2018-01-29 11:53:59 -0800

    MISC: Add beer products

--- sql/AbProductToSiqCatalog.sql
+++ sql/AbProductToSiqCatalog.sql
@@ -12,12 +12,35 @@ CASE
 WHEN category = 'Premium' THEN '04-02-00'
 WHEN category = 'Sub Premium' THEN '04-03-00'
 WHEN category = 'Imports' THEN '04-05-00'
-ELSE ''
+WHEN category = 'Specialty' THEN '04-01-00'
+WHEN category = 'Micro' THEN '04-06-00'
+WHEN category = 'Wine' THEN '05-00-00'
+WHEN category = 'FMB' THEN '04-09-00'
+WHEN category = 'Malt Liquor' THEN '04-07-00'
+WHEN category = 'Cider' THEN '04-07-00'
+WHEN category = 'Liquor' THEN '06-00-00'
+WHEN category = 'Cider' THEN '04-09-00'
+WHEN category = 'Wine Cooler' THEN '05-03-00'
+WHEN category = 'Malta' THEN '07-06-00'
+WHEN category = 'Non Alcohol' THEN '04-08-00'
+WHEN category = 'Specialty Soda' THEN '07-06-00'
+ELSE CONCAT(' ?? ',category)
 END as categorySubCode,
 CASE 
 WHEN category = 'Premium' THEN 'Premium'
 WHEN category = 'Sub Premium' THEN 'Popular'
 WHEN category = 'Imports' THEN 'Imports'
+WHEN category = 'Specialty' THEN 'Super Premium'
+WHEN category = 'Micro' THEN 'Microbrews/Craft'
+WHEN category = 'Wine' THEN 'Wine'
+WHEN category = 'FMB' THEN 'Flavored Malt'
+WHEN category = 'Cider' THEN 'Flavored Malt'
+WHEN category = 'Malt Liquor' THEN 'Malt Liquor'
+WHEN category = 'Liquor' THEN 'Liquor'
+WHEN category = 'Wine Cooler' THEN 'Coolers/Wine Cocktails'
+WHEN category = 'Malta' THEN 'Other Packaged Beverages (Non-alcoholic)'
+WHEN category = 'Non Alcohol' THEN 'Non-alcoholic'
+WHEN category = 'Specialty Soda' THEN 'Other Packaged Beverages (Non-alcoholic)'
 ELSE ''
 END as categorySubDescription,
 'Beer' as category,
@@ -28,6 +51,7 @@ segment as segment,
 container as container, size as size, uom as uom, '' as active,
 '' as privateLabelFlag, '' as	consumption, containerDescription	as package, '' as	flavor,
 brand as brand, brandType as brandType, ROUND(heigh,2) as height, ROUND(width,2) as width, ROUND(depth,2) as depth, shapeId as shapeId, family as family,
-'' as trademark, country, color, ROUND(alternateHeight,2) as alternateHeight, ROUND(alternateWeight,2) as alternateWidth, ROUND(alternateDepth,2) as alternateDepth,
+'' as trademark, country, '' as color, ROUND(alternateHeight,2) as alternateHeight, ROUND(alternateWeight,2) as alternateWidth, ROUND(alternateDepth,2) as alternateDepth,
 containerDescription, distributor, industryType, FORMAT_UTC_USEC(NOW()) as dateCreated
 FROM [default.AbProduct]
+WHERE category NOT IN ('Energy','Flex','Mixer','Non-Applicable','Non-Beer','Tea','Water','CSD')
