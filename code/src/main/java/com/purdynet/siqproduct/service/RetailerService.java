7bb99468994e670d91c0d390fb5d33db1f95ff8f
David Purdy <dnpurdy@gmail.com>
2018-05-18 00:42:52 -0400

    MISC: Fix up tests

--- code/src/main/java/com/purdynet/siqproduct/service/RetailerService.java
+++ code/src/main/java/com/purdynet/siqproduct/service/RetailerService.java
@@ -5,22 +5,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RetailerService {
-    public String progressSql(Retailer retailer) {
-        return progressSql(retailer, null, "");
-    }
+    //public String progressSql(Retailer retailer) {
+    //    return progressSql(retailer, null, "");
+    //}
 
-    public String progressSql(Retailer retailer, Integer limit) {
-        return progressSql(retailer, limit, "");
-    }
+    //public String progressSql(Retailer retailer, Integer limit) {
+    //    return progressSql(retailer, limit, "");
+    //}
 
     public String progressSql(Retailer retailer, Integer limit, String where) {
-        return "SELECT pp.itemId itemId, pp.manufacturer manufacturer, pp.retailerItemId retailerItemId, pp.per revPortion, NVL(c.description,pp.description) description, pp.deptDescription as retailerDept, c.category as nacsCategory, \n" +
-                " IF(c.description IS NULL,\"TODO\",\"COMPLETE\") as complete, IF(" + retailer.isUpcLogic() + ",\"UPC\",\"PLU\") isUpc,\n" +
-                " IF(c.description IS NULL,0,pp.per) as completeRevenue, IF(c.description IS NULL,pp.per,0) as incompleteRevenue, \n" +
-                " IF(c.description IS NULL,0,1) as completeItems, IF(c.description IS NULL,1,0) as incompleteItems,\n" +
-                " IF(c.description IS NULL,0,pp.perDept) as completeDeptRevenue, IF(c.description IS NULL,pp.perDept,0) as incompleteDeptRevenue FROM (" +
-                getMappedBase(retailer) + "\n" +
-                " ) pp LEFT OUTER JOIN [swiftiq-master:siq.Catalog] c ON pp.itemId = c.upc\n" +
+        return "SELECT pp.itemId itemId, pp.manufacturer manufacturer, pp.retailerItemId retailerItemId, pp.per revPortion, " +
+                "NVL(c.description,pp.description) description, pp.deptDescription as retailerDept, c.category as nacsCategory, " +
+                " IF(c.description IS NULL,\"TODO\",\"COMPLETE\") as complete, IF(" + retailer.isUpcLogic() + ",\"UPC\",\"PLU\") isUpc," +
+                " IF(c.description IS NULL,0,pp.per) as completeRevenue, IF(c.description IS NULL,pp.per,0) as incompleteRevenue, " +
+                " IF(c.description IS NULL,0,1) as completeItems, IF(c.description IS NULL,1,0) as incompleteItems, " +
+                " IF(c.description IS NULL,0,pp.perDept) as completeDeptRevenue, IF(c.description IS NULL,pp.perDept,0) as incompleteDeptRevenue FROM ( " +
+                getMappedBase(retailer) +
+                " ) pp LEFT OUTER JOIN [swiftiq-master:siq.Catalog] c ON pp.itemId = c.upc " +
                 where +
                 " ORDER BY pp.per DESC " + (limit != null ? " LIMIT " + limit + " " : "");
     }
@@ -33,6 +34,9 @@ public class RetailerService {
     }
 
     private String getBaseRev(Retailer retailer) {
-        return "SELECT a.itemId retailerItemId, "+retailer.fixedUpc()+" as itemId, "+retailer.mapJoin()+" as mapJoin, p.description as description, p.manufacturer as manufacturer, p.deptDescription as deptDescription, a.totalVal as totalVal FROM (SELECT itemId, SUM(ABS(quantity)*ABS(extendedAmount/1000)) totalVal FROM ["+retailer.projectId()+":default.LineItem] GROUP BY 1) a JOIN ["+retailer.projectId()+":default.Product] p ON a.itemId=p.upc";
+        return "SELECT a.itemId retailerItemId, "+retailer.fixedUpc()+" as itemId, "+retailer.mapJoin()+" as mapJoin, p.description as description, " +
+                "p.manufacturer as manufacturer, p.deptDescription as deptDescription, a.totalVal as totalVal FROM (" +
+                "SELECT itemId, SUM(ABS(quantity)*ABS(extendedAmount/1000)) totalVal FROM ["+retailer.projectId()+":default.LineItem] GROUP BY 1) a " +
+                "JOIN ["+retailer.projectId()+":default.Product] p ON a.itemId=p.upc";
     }
 }
--- code/src/test/java/com/purdynet/siqproduct/service/RetailerServiceTest.java
+++ code/src/test/java/com/purdynet/siqproduct/service/RetailerServiceTest.java
@@ -1,5 +1,6 @@
 package com.purdynet.siqproduct.service;
 
+import com.purdynet.siqproduct.retailer.KumAndGo;
 import com.purdynet.siqproduct.retailer.Retailer;
 import org.junit.Test;
 
@@ -11,60 +12,20 @@ public class RetailerServiceTest {
 
     @Test
     public void progressSql() {
-        String progressSql = retailerService.progressSql(getTestRetailer());
-        assertEquals("SQL dpesn't match!", "SELECT pp.itemId itemId, pp.manufacturer manufacturer, pp.retailerItemId retailerItemId, pp.per revPortion, NVL(c.description,pp.description) description, pp.deptDescription as retailerDept, c.category as nacsCategory, \n" +
-                " IF(c.description IS NULL,\"TODO\",\"COMPLETE\") as complete, IF( (LENGTH(pp.itemId) = 10 OR LENGTH(pp.itemId) == 11) ,\"UPC\",\"PLU\") isUpc,\n" +
-                " IF(c.description IS NULL,0,pp.per) as completeRevenue, IF(c.description IS NULL,pp.per,0) as incompleteRevenue, \n" +
-                " IF(c.description IS NULL,0,1) as completeItems, IF(c.description IS NULL,1,0) as incompleteItems,\n" +
-                " IF(c.description IS NULL,0,pp.perDept) as completeDeptRevenue, IF(c.description IS NULL,pp.perDept,0) as incompleteDeptRevenue FROM ( \n" +
-                "  SELECT NVL(rm.siqId, rev.itemId) itemId, rev.itemId retailerItemId, manufacturer, description, rev.deptDescription deptDescription, rev.totalVal,\n" +
-                "  RATIO_TO_REPORT(rev.totalVal) OVER () as per, RATIO_TO_REPORT(rev.totalVal) OVER (PARTITION BY deptDescription) as perDept FROM (\n" +
-                "   SELECT  itemId  as itemId, p.description, p.manufacturer, p.deptDescription, SUM(ABS(quantity)*ABS(extendedAmount/1000)) totalVal FROM [testretailer-siq:default.LineItem] li JOIN [testretailer-siq:default.Product] p ON li.itemId = p.upc GROUP BY 1,2,3,4\n" +
-                "  ) rev LEFT OUTER JOIN [testretailer-siq:siq.retailerMap] rm ON rev.itemId=rm.retailerId ORDER BY 4 DESC\n" +
-                " ) pp LEFT OUTER JOIN [swiftiq-master:siq.Catalog] c ON pp.itemId = c.upc\n" +
-                " ORDER BY pp.per DESC ", progressSql);
+        String progressSql = retailerService.progressSql(getTestRetailer(), null, "");
+        assertEquals("SQL dpesn't match!", "SELECT pp.itemId itemId, pp.manufacturer manufacturer, pp.retailerItemId retailerItemId, pp.per revPortion, NVL(c.description,pp.description) description, pp.deptDescription as retailerDept, c.category as nacsCategory,  IF(c.description IS NULL,\"TODO\",\"COMPLETE\") as complete, IF( (LENGTH(pp.itemId) = 10 OR LENGTH(pp.itemId) == 11) ,\"UPC\",\"PLU\") isUpc, IF(c.description IS NULL,0,pp.per) as completeRevenue, IF(c.description IS NULL,pp.per,0) as incompleteRevenue,  IF(c.description IS NULL,0,1) as completeItems, IF(c.description IS NULL,1,0) as incompleteItems,  IF(c.description IS NULL,0,pp.perDept) as completeDeptRevenue, IF(c.description IS NULL,pp.perDept,0) as incompleteDeptRevenue FROM ( SELECT NVL(rm.siqId,rev.itemId) itemId, rev.retailerItemId retailerItemId, manufacturer, description, rev.deptDescription deptDescription, rev.totalVal, RATIO_TO_REPORT(rev.totalVal) OVER () as per, RATIO_TO_REPORT(rev.totalVal) OVER (PARTITION BY deptDescription) as perDept FROM (SELECT a.itemId retailerItemId, p.upc as itemId, p.upc as mapJoin, p.description as description, p.manufacturer as manufacturer, p.deptDescription as deptDescription, a.totalVal as totalVal FROM (SELECT itemId, SUM(ABS(quantity)*ABS(extendedAmount/1000)) totalVal FROM [testretailer-siq:default.LineItem] GROUP BY 1) a JOIN [testretailer-siq:default.Product] p ON a.itemId=p.upc) rev LEFT OUTER JOIN [testretailer-siq:siq.retailerMap] rm ON rev.mapJoin=rm.retailerId ORDER BY 6 DESC ) pp LEFT OUTER JOIN [swiftiq-master:siq.Catalog] c ON pp.itemId = c.upc  ORDER BY pp.per DESC ", progressSql);
 
-        String progressSql2 = retailerService.progressSql(getTestRetailer2());
-        assertEquals("SQL dpesn't match!", "SELECT pp.itemId itemId, pp.manufacturer manufacturer, pp.retailerItemId retailerItemId, pp.per revPortion, NVL(c.description,pp.description) description, pp.deptDescription as retailerDept, c.category as nacsCategory, \n" +
-                " IF(c.description IS NULL,\"TODO\",\"COMPLETE\") as complete, IF( (LENGTH(pp.itemId) = 10 OR LENGTH(pp.itemId) == 11) ,\"UPC\",\"PLU\") isUpc,\n" +
-                " IF(c.description IS NULL,0,pp.per) as completeRevenue, IF(c.description IS NULL,pp.per,0) as incompleteRevenue, \n" +
-                " IF(c.description IS NULL,0,1) as completeItems, IF(c.description IS NULL,1,0) as incompleteItems,\n" +
-                " IF(c.description IS NULL,0,pp.perDept) as completeDeptRevenue, IF(c.description IS NULL,pp.perDept,0) as incompleteDeptRevenue FROM ( \n" +
-                "  SELECT NVL(rm.siqId, rev.itemId) itemId, rev.itemId retailerItemId, manufacturer, description, rev.deptDescription deptDescription, rev.totalVal,\n" +
-                "  RATIO_TO_REPORT(rev.totalVal) OVER () as per, RATIO_TO_REPORT(rev.totalVal) OVER (PARTITION BY deptDescription) as perDept FROM (\n" +
-                "   SELECT  li.productId  as itemId, p.description, p.manufacturer, p.deptDescription, SUM(ABS(quantity)*ABS(extendedAmount/1000)) totalVal FROM [testretailer-siq:default.LineItem] li JOIN [testretailer-siq:default.Product] p ON li.itemId = p.upc GROUP BY 1,2,3,4\n" +
-                "  ) rev LEFT OUTER JOIN [testretailer-siq:siq.retailerMap] rm ON rev.itemId=rm.retailerId ORDER BY 4 DESC\n" +
-                " ) pp LEFT OUTER JOIN [swiftiq-master:siq.Catalog] c ON pp.itemId = c.upc\n" +
-                " ORDER BY pp.per DESC ", progressSql2);
+        String progressSql2 = retailerService.progressSql(getTestRetailer2(), null, "");
+        assertEquals("SQL dpesn't match!", "SELECT pp.itemId itemId, pp.manufacturer manufacturer, pp.retailerItemId retailerItemId, pp.per revPortion, NVL(c.description,pp.description) description, pp.deptDescription as retailerDept, c.category as nacsCategory,  IF(c.description IS NULL,\"TODO\",\"COMPLETE\") as complete, IF( (LENGTH(pp.itemId) = 10 OR LENGTH(pp.itemId) == 11) ,\"UPC\",\"PLU\") isUpc, IF(c.description IS NULL,0,pp.per) as completeRevenue, IF(c.description IS NULL,pp.per,0) as incompleteRevenue,  IF(c.description IS NULL,0,1) as completeItems, IF(c.description IS NULL,1,0) as incompleteItems,  IF(c.description IS NULL,0,pp.perDept) as completeDeptRevenue, IF(c.description IS NULL,pp.perDept,0) as incompleteDeptRevenue FROM ( SELECT NVL(rm.siqId,rev.itemId) itemId, rev.retailerItemId retailerItemId, manufacturer, description, rev.deptDescription deptDescription, rev.totalVal, RATIO_TO_REPORT(rev.totalVal) OVER () as per, RATIO_TO_REPORT(rev.totalVal) OVER (PARTITION BY deptDescription) as perDept FROM (SELECT a.itemId retailerItemId, p.upc as itemId, p.productId as mapJoin, p.description as description, p.manufacturer as manufacturer, p.deptDescription as deptDescription, a.totalVal as totalVal FROM (SELECT itemId, SUM(ABS(quantity)*ABS(extendedAmount/1000)) totalVal FROM [testretailer-siq:default.LineItem] GROUP BY 1) a JOIN [testretailer-siq:default.Product] p ON a.itemId=p.upc) rev LEFT OUTER JOIN [testretailer-siq:siq.retailerMap] rm ON rev.mapJoin=rm.retailerId ORDER BY 6 DESC ) pp LEFT OUTER JOIN [swiftiq-master:siq.Catalog] c ON pp.itemId = c.upc  ORDER BY pp.per DESC ", progressSql2);
     }
 
     @Test
     public void progressSql1() {
-        String progressSql = retailerService.progressSql(getTestRetailer(), 1000);
-        assertEquals("SQL dpesn't match!", "SELECT pp.itemId itemId, pp.manufacturer manufacturer, pp.retailerItemId retailerItemId, pp.per revPortion, NVL(c.description,pp.description) description, pp.deptDescription as retailerDept, c.category as nacsCategory, \n" +
-                " IF(c.description IS NULL,\"TODO\",\"COMPLETE\") as complete, IF( (LENGTH(pp.itemId) = 10 OR LENGTH(pp.itemId) == 11) ,\"UPC\",\"PLU\") isUpc,\n" +
-                " IF(c.description IS NULL,0,pp.per) as completeRevenue, IF(c.description IS NULL,pp.per,0) as incompleteRevenue, \n" +
-                " IF(c.description IS NULL,0,1) as completeItems, IF(c.description IS NULL,1,0) as incompleteItems,\n" +
-                " IF(c.description IS NULL,0,pp.perDept) as completeDeptRevenue, IF(c.description IS NULL,pp.perDept,0) as incompleteDeptRevenue FROM ( \n" +
-                "  SELECT NVL(rm.siqId, rev.itemId) itemId, rev.itemId retailerItemId, manufacturer, description, rev.deptDescription deptDescription, rev.totalVal,\n" +
-                "  RATIO_TO_REPORT(rev.totalVal) OVER () as per, RATIO_TO_REPORT(rev.totalVal) OVER (PARTITION BY deptDescription) as perDept FROM (\n" +
-                "   SELECT  itemId  as itemId, p.description, p.manufacturer, p.deptDescription, SUM(ABS(quantity)*ABS(extendedAmount/1000)) totalVal FROM [testretailer-siq:default.LineItem] li JOIN [testretailer-siq:default.Product] p ON li.itemId = p.upc GROUP BY 1,2,3,4\n" +
-                "  ) rev LEFT OUTER JOIN [testretailer-siq:siq.retailerMap] rm ON rev.itemId=rm.retailerId ORDER BY 4 DESC\n" +
-                " ) pp LEFT OUTER JOIN [swiftiq-master:siq.Catalog] c ON pp.itemId = c.upc\n" +
-                " ORDER BY pp.per DESC  LIMIT 1000 ", progressSql);
+        String progressSql = retailerService.progressSql(getTestRetailer(), 1000, "");
+        assertEquals("SQL dpesn't match!", "SELECT pp.itemId itemId, pp.manufacturer manufacturer, pp.retailerItemId retailerItemId, pp.per revPortion, NVL(c.description,pp.description) description, pp.deptDescription as retailerDept, c.category as nacsCategory,  IF(c.description IS NULL,\"TODO\",\"COMPLETE\") as complete, IF( (LENGTH(pp.itemId) = 10 OR LENGTH(pp.itemId) == 11) ,\"UPC\",\"PLU\") isUpc, IF(c.description IS NULL,0,pp.per) as completeRevenue, IF(c.description IS NULL,pp.per,0) as incompleteRevenue,  IF(c.description IS NULL,0,1) as completeItems, IF(c.description IS NULL,1,0) as incompleteItems,  IF(c.description IS NULL,0,pp.perDept) as completeDeptRevenue, IF(c.description IS NULL,pp.perDept,0) as incompleteDeptRevenue FROM ( SELECT NVL(rm.siqId,rev.itemId) itemId, rev.retailerItemId retailerItemId, manufacturer, description, rev.deptDescription deptDescription, rev.totalVal, RATIO_TO_REPORT(rev.totalVal) OVER () as per, RATIO_TO_REPORT(rev.totalVal) OVER (PARTITION BY deptDescription) as perDept FROM (SELECT a.itemId retailerItemId, p.upc as itemId, p.upc as mapJoin, p.description as description, p.manufacturer as manufacturer, p.deptDescription as deptDescription, a.totalVal as totalVal FROM (SELECT itemId, SUM(ABS(quantity)*ABS(extendedAmount/1000)) totalVal FROM [testretailer-siq:default.LineItem] GROUP BY 1) a JOIN [testretailer-siq:default.Product] p ON a.itemId=p.upc) rev LEFT OUTER JOIN [testretailer-siq:siq.retailerMap] rm ON rev.mapJoin=rm.retailerId ORDER BY 6 DESC ) pp LEFT OUTER JOIN [swiftiq-master:siq.Catalog] c ON pp.itemId = c.upc  ORDER BY pp.per DESC  LIMIT 1000 ", progressSql);
 
-        String progressSql2 = retailerService.progressSql(getTestRetailer2(), 1000);
-        assertEquals("SQL dpesn't match!", "SELECT pp.itemId itemId, pp.manufacturer manufacturer, pp.retailerItemId retailerItemId, pp.per revPortion, NVL(c.description,pp.description) description, pp.deptDescription as retailerDept, c.category as nacsCategory, \n" +
-                " IF(c.description IS NULL,\"TODO\",\"COMPLETE\") as complete, IF( (LENGTH(pp.itemId) = 10 OR LENGTH(pp.itemId) == 11) ,\"UPC\",\"PLU\") isUpc,\n" +
-                " IF(c.description IS NULL,0,pp.per) as completeRevenue, IF(c.description IS NULL,pp.per,0) as incompleteRevenue, \n" +
-                " IF(c.description IS NULL,0,1) as completeItems, IF(c.description IS NULL,1,0) as incompleteItems,\n" +
-                " IF(c.description IS NULL,0,pp.perDept) as completeDeptRevenue, IF(c.description IS NULL,pp.perDept,0) as incompleteDeptRevenue FROM ( \n" +
-                "  SELECT NVL(rm.siqId, rev.itemId) itemId, rev.itemId retailerItemId, manufacturer, description, rev.deptDescription deptDescription, rev.totalVal,\n" +
-                "  RATIO_TO_REPORT(rev.totalVal) OVER () as per, RATIO_TO_REPORT(rev.totalVal) OVER (PARTITION BY deptDescription) as perDept FROM (\n" +
-                "   SELECT  li.productId  as itemId, p.description, p.manufacturer, p.deptDescription, SUM(ABS(quantity)*ABS(extendedAmount/1000)) totalVal FROM [testretailer-siq:default.LineItem] li JOIN [testretailer-siq:default.Product] p ON li.itemId = p.upc GROUP BY 1,2,3,4\n" +
-                "  ) rev LEFT OUTER JOIN [testretailer-siq:siq.retailerMap] rm ON rev.itemId=rm.retailerId ORDER BY 4 DESC\n" +
-                " ) pp LEFT OUTER JOIN [swiftiq-master:siq.Catalog] c ON pp.itemId = c.upc\n" +
-                " ORDER BY pp.per DESC  LIMIT 1000 ", progressSql2);
+        String progressSql2 = retailerService.progressSql(getTestRetailer2(), 1000, "");
+        assertEquals("SQL dpesn't match!", "SELECT pp.itemId itemId, pp.manufacturer manufacturer, pp.retailerItemId retailerItemId, pp.per revPortion, NVL(c.description,pp.description) description, pp.deptDescription as retailerDept, c.category as nacsCategory,  IF(c.description IS NULL,\"TODO\",\"COMPLETE\") as complete, IF( (LENGTH(pp.itemId) = 10 OR LENGTH(pp.itemId) == 11) ,\"UPC\",\"PLU\") isUpc, IF(c.description IS NULL,0,pp.per) as completeRevenue, IF(c.description IS NULL,pp.per,0) as incompleteRevenue,  IF(c.description IS NULL,0,1) as completeItems, IF(c.description IS NULL,1,0) as incompleteItems,  IF(c.description IS NULL,0,pp.perDept) as completeDeptRevenue, IF(c.description IS NULL,pp.perDept,0) as incompleteDeptRevenue FROM ( SELECT NVL(rm.siqId,rev.itemId) itemId, rev.retailerItemId retailerItemId, manufacturer, description, rev.deptDescription deptDescription, rev.totalVal, RATIO_TO_REPORT(rev.totalVal) OVER () as per, RATIO_TO_REPORT(rev.totalVal) OVER (PARTITION BY deptDescription) as perDept FROM (SELECT a.itemId retailerItemId, p.upc as itemId, p.productId as mapJoin, p.description as description, p.manufacturer as manufacturer, p.deptDescription as deptDescription, a.totalVal as totalVal FROM (SELECT itemId, SUM(ABS(quantity)*ABS(extendedAmount/1000)) totalVal FROM [testretailer-siq:default.LineItem] GROUP BY 1) a JOIN [testretailer-siq:default.Product] p ON a.itemId=p.upc) rev LEFT OUTER JOIN [testretailer-siq:siq.retailerMap] rm ON rev.mapJoin=rm.retailerId ORDER BY 6 DESC ) pp LEFT OUTER JOIN [swiftiq-master:siq.Catalog] c ON pp.itemId = c.upc  ORDER BY pp.per DESC  LIMIT 1000 ", progressSql2);
     }
 
     private Retailer getTestRetailer() {
@@ -136,6 +97,11 @@ public class RetailerServiceTest {
             @Override
             public String tobaccoClause() {
                 return "";
+            }
+
+            @Override
+            public String mapJoin() {
+                return "p.productId";
             }
         };
     }
