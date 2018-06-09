b277d93add5b0551ad4a34cc99cba69a56f7dfbe
David Purdy <dnpurdy@gmail.com>
2018-06-09 10:09:48 -0700

    Allow setup of missing controller to filter by retailer on query param

--- code/src/main/java/com/purdynet/siqproduct/controller/MissingController.java
+++ code/src/main/java/com/purdynet/siqproduct/controller/MissingController.java
@@ -7,17 +7,18 @@ import com.purdynet.siqproduct.model.retailer.Retailer;
 import com.purdynet.siqproduct.service.FreemarkerService;
 import com.purdynet.siqproduct.service.ProductService;
 import com.purdynet.siqproduct.service.RetailerService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static com.purdynet.siqproduct.biqquery.BQClient.convertTableRowToModel;
 import static com.purdynet.siqproduct.biqquery.BQClient.runQuerySync;
@@ -43,53 +44,53 @@ public class MissingController {
     }
 
     @RequestMapping(value = {"/missing","/missing/{upc}"}, produces = MediaType.TEXT_HTML_VALUE)
-    public String missingHTML(@PathVariable(name = "upc", required = false) String upc) {
-        return getAGHtml("/missing" + (upc != null ? "/"+upc : ""));
+    public String missingHTML(@PathVariable(name = "upc", required = false) String upc, @RequestParam(name = "retailers", required = false) String retailers) {
+        return getAGHtml("/missing" + (upc != null ? "/"+upc : "") + (retailers !=null ? "?retailers="+retailers : ""));
     }
 
     @RequestMapping(value = {"/missing","/missing/{upc}"}, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<MissingItem> missingJson(@PathVariable(name = "upc", required = false) String upc) {
-        return makeMissingItemList(upc, Retailer::allClause);
+    public List<MissingItem> missingJson(@PathVariable(name = "upc", required = false) String upc, @RequestParam(name = "retailers", required = false) String retailers) {
+        return makeMissingItemList(upc, retailers, Retailer::allClause);
     }
 
     @RequestMapping(value = {"/missing-beer","/missing-beer/{upc}"}, produces = MediaType.TEXT_HTML_VALUE)
-    public String missingBeerHTML(@PathVariable(name = "upc", required = false) String upc) {
-        return getAGHtml("/missing-beer" + (upc != null ? "/"+upc : ""));
+    public String missingBeerHTML(@PathVariable(name = "upc", required = false) String upc, @RequestParam(name = "retailers", required = false) String retailers) {
+        return getAGHtml("/missing-beer" + (upc != null ? "/"+upc : "") + (retailers !=null ? "?retailers="+retailers : ""));
     }
 
     @RequestMapping(value = {"/missing-beer","/missing-beer/{upc}"}, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<MissingItem> missingBeerJson(@PathVariable(name = "upc", required = false) String upc) {
-        return makeMissingItemList(upc, Retailer::beerClause);
+    public List<MissingItem> missingBeerJson(@PathVariable(name = "upc", required = false) String upc, @RequestParam(name = "retailers", required = false) String retailers) {
+        return makeMissingItemList(upc, retailers, Retailer::beerClause);
     }
 
     @RequestMapping(value = {"/missing-beverage","/missing-beverage/{upc}"}, produces = MediaType.TEXT_HTML_VALUE)
-    public String missingBeverageHTML(@PathVariable(name = "upc", required = false) String upc) {
-        return getAGHtml("/missing-beverage" + (upc != null ? "/"+upc : ""));
+    public String missingBeverageHTML(@PathVariable(name = "upc", required = false) String upc, @RequestParam(name = "retailers", required = false) String retailers) {
+        return getAGHtml("/missing-beverage" + (upc != null ? "/"+upc : "") + (retailers !=null ? "?retailers="+retailers : ""));
     }
 
     @RequestMapping(value = {"/missing-beverage","/missing-beverage/{upc}"}, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<MissingItem> missingBeverageJson(@PathVariable(name = "upc", required = false) String upc) {
-        return makeMissingItemList(upc, Retailer::beverageClause);
+    public List<MissingItem> missingBeverageJson(@PathVariable(name = "upc", required = false) String upc, @RequestParam(name = "retailers", required = false) String retailers) {
+        return makeMissingItemList(upc, retailers, Retailer::beverageClause);
     }
 
     @RequestMapping(value = {"/missing-tobacco","/missing-tobacco/{upc}"}, produces = MediaType.TEXT_HTML_VALUE)
-    public String missingTobaccoHTML(@PathVariable(name = "upc", required = false) String upc) {
-        return getAGHtml("/missing-tobacco" + (upc != null ? "/"+upc : ""));
+    public String missingTobaccoHTML(@PathVariable(name = "upc", required = false) String upc, @RequestParam(name = "retailers", required = false) String retailers) {
+        return getAGHtml("/missing-tobacco" + (upc != null ? "/"+upc : "") + (retailers !=null ? "?retailers="+retailers : ""));
     }
 
     @RequestMapping(value = {"/missing-tobacco","/missing-tobacco/{upc}"}, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<MissingItem> missingTobaccoJson(@PathVariable(name = "upc", required = false) String upc) {
-        return makeMissingItemList(upc, Retailer::tobaccoClause);
+    public List<MissingItem> missingTobaccoJson(@PathVariable(name = "upc", required = false) String upc, @RequestParam(name = "retailers", required = false) String retailers) {
+        return makeMissingItemList(upc, retailers, Retailer::tobaccoClause);
     }
 
     @RequestMapping(value = {"/missing-saltysnack","/missing-saltysnack/{upc}"}, produces = MediaType.TEXT_HTML_VALUE)
-    public String missingSaltySnackHTML(@PathVariable(name = "upc", required = false) String upc) {
-        return getAGHtml("/missing-saltysnack" + (upc != null ? "/"+upc : ""));
+    public String missingSaltySnackHTML(@PathVariable(name = "upc", required = false) String upc, @RequestParam(name = "retailers", required = false) String retailers) {
+        return getAGHtml("/missing-saltysnack" + (upc != null ? "/"+upc : "") + (retailers !=null ? "?retailers="+retailers : ""));
     }
 
     @RequestMapping(value = {"/missing-saltysnack","/missing-saltysnack/{upc}"}, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<MissingItem> missingSaltySnackJson(@PathVariable(name = "upc", required = false) String upc) {
-        return makeMissingItemList(upc, Retailer::saltySnacksClause);
+    public List<MissingItem> missingSaltySnackJson(@PathVariable(name = "upc", required = false) String upc, @RequestParam(name = "retailers", required = false) String retailers) {
+        return makeMissingItemList(upc, retailers, Retailer::saltySnacksClause);
     }
 
     private String getAGHtml(final String uri) {
@@ -99,9 +100,14 @@ public class MissingController {
         return freemarkerService.processTemplate("templates/AGGridPage.ftl", dataModel);
     }
 
-    private List<MissingItem> makeMissingItemList(String upc, Function<Retailer, String> productSelectFnc) {
-        BQClient BQClient = runQuerySync(projectId, productService.productProgress(retailerService.getRetailers(), productSelectFnc, upc));
+    private List<MissingItem> makeMissingItemList(final String upc, final String retailers, final Function<Retailer, String> productSelectFnc) {
+        BQClient BQClient = runQuerySync(projectId, productService.productProgress(filterRetailers(retailerService.getRetailers(),retailers), productSelectFnc, upc));
         return makeMissingItemList(BQClient.getBqTableData());
+    }
+
+    private List<Retailer> filterRetailers(final List<Retailer> retailerList, final String retailersPicked) {
+        if (StringUtils.isEmpty(retailersPicked)) return retailerList;
+        else return retailerList.stream().filter(r -> new HashSet<String>(Arrays.asList(retailersPicked.split(","))).contains(r.projectId())).collect(Collectors.toList());
     }
 
     private List<MissingItem> makeMissingItemList(BqTableData bqTableData) {
